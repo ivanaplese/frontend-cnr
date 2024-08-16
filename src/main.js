344cccd7ed1108901216e694255ad26a569a464b
ivanaplese <ivanaplese123@gmail.com>
2024-08-16 21:36:24 +0200

    Povezivanje backenda i frontenda- axios i vercel

--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 
-createApp(App).use(router).mount('#app')
+// createApp(App).use(router).mount('#app')
 
-// Postavite Axios kao globalni resurs
-app.config.globalProperties.$axios = axios
+// // Postavite Axios kao globalni resurs
+// app.config.globalProperties.$axios = axios
 
+axios.defaults.baseURL = 'https://cars-n-rides-backend.vercel.app';
+
+// Stvori Vue aplikaciju
+const app = createApp(App);
+
+// Registriraj axios kao globalni resurs
+app.config.globalProperties.$axios = axios;
+
+app.use(router).mount('#app');
