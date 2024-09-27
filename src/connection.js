f520133f11129d359293cc635dfe259f59d2d790
ivanaplese <ivanaplese123@gmail.com>
2024-09-27 16:15:54 +0200

    update da proradi sve šta je prije radilo

--- src/connection.js
+++ src/connection.js
@@ -1,11 +1,26 @@
-import axios from "axios";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
 
-const apiClient = axios.create({
-    baseURL: "https://cars-n-rides.netlify.app/",
-    headers: {
-        "Content-Type": "application/json",
-    },
-});
+dotenv.config();
 
-export default apiClient;
+const url = process.env.MONGO_URI; // This matches the updated variable name in the .env file
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let db;
+
+async function connectDB() {
+    if (!db) {
+        try {
+            await client.connect();
+            db = client.db(process.env.DB_NAME); // Make sure DB_NAME is also set in your .env file if you're using it
+            console.log("Connected to MongoDB");
+        } catch (error) {
+            console.error("Failed to connect to MongoDB", error);
+            throw error;
+        }
+    }
+    return db;
+}
+
+export default connectDB;
 
