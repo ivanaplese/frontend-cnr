9c42b5a1a3653a44f9224beea78a60e2103b5abf
ivanaplese <ivanaplese123@gmail.com>
2024-09-30 00:02:50 +0200

    render proba

--- src/apiClient.js
+++ src/apiClient.js
@@ -1,10 +1,10 @@
-// import axios from "axios";
+import axios from "axios";
 
-// const apiClient = axios.create({
-//     baseURL: "https://backend-cnr.onrender.com",
-//     headers: {
-//         "Content-Type": "application/json",
-//     },
-// });
+const apiClient = axios.create({
+    baseURL: "https://backend-cnr.onrender.com",
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
 
-// export default apiClient;
+export default apiClient;
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -30,7 +30,6 @@ export default {
       try {
       
         const response = await this.$axios.post(`${process.env.VUE_APP_API_URL}/api/login`, {
-
           // const response = await this.$axios.post("http://localhost:8080/api/login", {
 
           username: this.username,
